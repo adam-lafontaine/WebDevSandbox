78feb0cf0a609e4ad6a2748b76856e858d4023e2
adam-lafontaine <adam.lafontaine@gmail.com>
2023-09-23 09:22:26 -0400

    add footer

--- VitePinaBootstrap/src/App.vue
+++ VitePinaBootstrap/src/App.vue
@@ -12,21 +12,36 @@ const menu_items = ref([
 
 <template>
 <nav class="navbar navbar-expand-lg navbar-dark nav-color">
-  <div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarLinks" aria-controls="navbarLinks" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarLinks">
-      <a class="navbar-brand" href="#">Hidden brand</a>
-      <ul class="navbar-nav" v-for="item in menu_items" :key="item.id">
-        <li class="nav-item">
-          <RouterLink class="nav-link" :to="item.route">{{ item.label }}</RouterLink>
-        </li>
-      </ul>
+    <div class="container-fluid">
+        <button 
+            class="navbar-toggler" 
+            type="button" 
+            data-bs-toggle="collapse" 
+            ata-bs-target="#navbarLinks" 
+            aria-controls="navbarLinks" 
+            aria-expanded="false" 
+            aria-label="Toggle navigation"
+        >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarLinks">
+        <a class="navbar-brand" href="#">Brand</a>
+        <ul class="navbar-nav" v-for="item in menu_items" :key="item.id">
+            <li class="nav-item">
+                <RouterLink class="nav-link" :to="item.route">{{ item.label }}</RouterLink>
+            </li>
+        </ul>
+        </div>
     </div>
-  </div>
 </nav>
-<RouterView></RouterView>
+<div class="container">
+    <RouterView></RouterView>
+</div>
+<footer class="bg-info text-center text-lg-start fixed-bottom nav-color footer-padding">
+    <div>
+        Footer
+    </div>
+</footer>
 </template>
 
 
--- VitePinaBootstrap/src/components/About.vue
+++ VitePinaBootstrap/src/components/About.vue
@@ -1,10 +1,49 @@
 <script setup lang="ts">
+import { useCountingStore } from '../stores/CountingStore'
+
+
+const counter = useCountingStore();
 
 </script>
 
 
 <template>
 <h1>About</h1>
+
+<div class="card" style="width: 18rem;">
+    <div class="card-body">
+        <h5 class="card-title">Card Title</h5>
+        <div class="row">
+            <div class="col-12">
+                Current Count: {{ counter.get_count }}
+            </div>
+            <div class="col-12">
+                Current Count: {{ counter.get_count_str }}
+            </div>
+            <div class="col-12">
+                <button 
+                    type="button" 
+                    class="btn btn-primary"
+                    @click="counter.increment"
+                >
+                    Plus
+                </button>
+                <button 
+                    type="button" 
+                    class="btn btn-primary"
+                    style="margin-left: 0.5rem"
+                    @click="counter.decrement"
+                >
+                    Minus
+                </button>
+            </div>
+        </div>
+        
+
+        
+    </div>
+</div>
+    
 </template>
 
 
